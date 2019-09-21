22517f57b5eb3ede0bb93aea4153d9d73814c938
Hridya Valsaraju <hridya@google.com>
2019-09-21 22:42:43 +0200

    sdm660-common: Build bootctrl using Soong
    This is required to build the recovery variant of the
    same to be used by fastbootd.
    
    Bug: 78793464
    Test: make
    Bug: 34254109
    Change-Id: I16eddcb853bda1a9969810f258884ebdbbfc1f5c
    Merged-In: I16eddcb853bda1a9969810f258884ebdbbfc1f5c
    (cherry picked from commit 805edb72d0bca42c59421440a896829e9a30c574)

--- common.mk
+++ common.mk
@@ -76,6 +76,7 @@ PRODUCT_PACKAGES += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
     android.hardware.boot@1.0-service
 
 # Camera
